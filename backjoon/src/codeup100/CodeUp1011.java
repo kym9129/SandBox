7d514ae3f11a0408fab132bca3d097fb1ef17237
kym9129 <kym9129@naver.com>
2021-01-28 00:08:03 +0900

    edited class name

--- backjoon/src/codeup100/CodeUp1011.java
+++ backjoon/src/codeup100/CodeUp1011.java
@@ -2,7 +2,7 @@ package codeup100;
 
 import java.util.Scanner;
 
-public class Codeup1011 {
+public class CodeUp1011 {
 	
 	//문자형(char)으로 변수를 하나 선언하고, 변수에 문자를 저장한 후
 	//변수에 저장되어 있는 문자를 그대로 출력해보자.
--- backjoon/src/codeup100/CodeUp1013.java
+++ backjoon/src/codeup100/CodeUp1013.java
@@ -2,7 +2,7 @@ package codeup100;
 
 import java.util.Scanner;
 
-public class Codeup1013 {
+public class CodeUp1013 {
 	
 	//정수(int) 2개를 입력받아 그대로 출력해보자.
 
